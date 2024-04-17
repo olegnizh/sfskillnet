fe691f659a1fe3cdb46dbe0aa3a62aafd76e1ef6
olegnizh <olegnizh@mail.ru>
2024-04-17 21:57:05 +0300

    appsettings.json configuration

---
+++ ConsoleAppConfiguration/ConsoleAppConfiguration/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace ConsoleAppConfiguration
+{
+    public class Program
+    {
+
+        static void Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            // https://stackoverflow.com/questions/31453495/how-to-read-appsettings-values-from-a-json-file-in-asp-net-core
+            // .NET Core 3.0 / .NET 6
+
+            //var AppName = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["APP_Name"];
+            //var ccc = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build().GetSection("AppSettings")["ccc"];
+
+            var MyConfig = new ConfigurationBuilder().AddJsonFile("appsettings.json").Build();
+            var IntExample = MyConfig.GetValue<int>("AppSettings:SampleIntValue");
+            var AppName = MyConfig.GetValue<string>("AppSettings:APP_Name");
+            var ccc = MyConfig.GetValue<string>("AppSettings:ccc");
+
+            Console.WriteLine(AppName);
+            Console.WriteLine(IntExample);
+            Console.WriteLine(ccc);
+
+            Console.ReadKey();
+
+        }
+
+
+
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# 1. CreateXmlSimple - DataTable to xml - NET 6
-# 2. ReadXmlSimple - xml to DataTable - NET 6
-# 3. WinFormsAppEmail - send message to email - NET 6
+# skill heap
+
