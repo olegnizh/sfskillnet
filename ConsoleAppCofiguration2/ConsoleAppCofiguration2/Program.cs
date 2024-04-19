f8ba87ba7aa4bdf024808065788a52bb0fafedf2
olegnizh <olegnizh@mail.ru>
2024-04-19 05:46:13 +0300

    appsettings.json configuration 2

---
+++ ConsoleAppCofiguration2/ConsoleAppCofiguration2/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+
+// https://www.tektutorialshub.com/entity-framework-core/ef-core-database-connection-string/
+
+var builder = new ConfigurationBuilder();
+builder.AddJsonFile("appsettings.json", optional: false);
+
+var configuration = builder.Build();
+
+var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+Console.WriteLine(connectionString);
+Console.ReadKey();
