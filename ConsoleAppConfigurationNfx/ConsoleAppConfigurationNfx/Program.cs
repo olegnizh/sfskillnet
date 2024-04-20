4f27247fda96d527420296af67d69bab37b231f4
olegnizh <olegnizh@mail.ru>
2024-04-20 06:10:23 +0300

    app configuration nfx

---
+++ ConsoleAppConfigurationNfx/ConsoleAppConfigurationNfx/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+
+namespace ConsoleAppConfigurationNfx
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var connectionString = ConfigurationSettings.AppSettings["StringConnection"];
+            Console.WriteLine(connectionString);
+			
+            var qa_table = ConfigurationSettings.AppSettings["qatable"];
+            Console.WriteLine(qa_table);
+
+            Console.ReadKey();
+
+        }
+
+
+    }
+}
