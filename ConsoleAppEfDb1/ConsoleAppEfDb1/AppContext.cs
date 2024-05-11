8bd89754c80ac3aff72d1291e84f3c8a31253516
olegnizh <olegnizh@mail.ru>
2024-05-11 18:58:13 +0300

    ef core sf 1

---
+++ ConsoleAppEfDb1/ConsoleAppEfDb1/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppEfDb1
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var ConStr = "server=localhost;port=3306;database=db1;user=root;password=;";
+            optionsBuilder.UseMySql(ConStr, ServerVersion.AutoDetect(ConStr));
+
+        }
+    }
+}
---
+++ ConsoleAppEfDb1/ConsoleAppEfDb1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql;
+
+namespace ConsoleAppEfDb1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //var db = new AppContext();
+            using (var db = new AppContext())
+            {
+                var user1 = new User { Name = "Arthur", Email = "h", Role = "Admin" };
+                var user2 = new User { Name = "klim", Email = "g", Role = "User" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+
+            }
+
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
+
+        }
+    }
+}
