983d33f5065669f330a507f54261301610f184a9
olegnizh <olegnizh@mail.ru>
2024-05-12 09:01:09 +0300

    sf - ef core 1

---
+++ EFDBsf1/AppContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDBsf1
+{
+    public class AppContext : DbContext
+    {
+        // Объекты таблицы Users
+        public DbSet<User> Users { get; set; }
+
+        public AppContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {			
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=test1;Trusted_Connection=True;");  
+
+        }
+    }
+}
---
+++ EFDBsf1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.Intrinsics.X86;
+
+namespace EFDBsf1
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, EF Core!");
+            Console.WriteLine("View users ...");
+
+            using (var db = new AppContext())
+            {
+                var user1 = new User { Name = "Arthur", Email = "", Role = "Admin" };
+                var user2 = new User { Name = "klim", Email = "", Role = "User" };
+                var user3 = new User { Name = "Job", Email = "", Role = "User1" };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.Users.Add(user3);
+
+                //db.Users.Remove(user3);
+
+                db.SaveChanges();
+
+                var users = db.Users.ToList();
+                foreach (var user in users)
+                {
+                    // Вывод Id пользователей
+                    Console.WriteLine($"{user.Id} - {user.Name} {user.Email} - {user.Role}");
+                }
+
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
